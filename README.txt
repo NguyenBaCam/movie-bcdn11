591eaa95f20bd17ea62b95087404ad5d33fea430
NguyenBaCam <nguyenbacam.nbc07@gmail.com>
2024-07-25 20:22:32 +0700

    Setup css

---
+++ README.txt
@@ -0,0 +1,8 @@
+1. Deploy(v)
+2. Caì thư viện
+    - Axios: gọi API
+    - React-router-dom: Chuyển trang
+    - Css: tailwindcss, styled-compoment, stylex, emotion,...
+    -lib UI: antd, master-ui, chraka-ui, radix,..
+    -Manage: redux, Zustand, recoil, Mobx,..
+    -Sever State: RTK Query, React-Query,...
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import "./config/style/modern-normalize.css";
+import "./config/style/tailwind.css";
+import "./config/style/font-family.css";
+import "./config/style/main.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
